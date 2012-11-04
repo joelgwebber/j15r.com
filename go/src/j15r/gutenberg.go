7eecd880a468916d11a0c2bff46f0014754fa3cc
Joel Webber <jgw@pobox.com>
2012-11-04 15:48:07 -0500

    More fixes. Should now fetch gutenberg files from somewhere that won't shit on us.

--- go/src/j15r/gutenberg.go
+++ go/src/j15r/gutenberg.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"errors"
+	"log"
+	"fmt"
 	"strconv"
+	"bytes"
 	"strings"
 	"io/ioutil"
 	"net/http"
+	"archive/zip"
 	"github.com/kellegous/pork"
 )
 
@@ -17,21 +20,72 @@ type Book []string
 
 var bookCache map[string]Book
 
-func readBook(bookId string) (Book, error) {
-	// Fetch from Gutenberg.
-	rsp, err := http.Get("http://www.gutenberg.org/ebooks/" + bookId + ".txt.utf-8")
-	if err != nil {
-		return nil, err
+func readFromGutenberg(bookId string) ([]byte, error) {
+	log.Printf("Fetching %v from Gutenberg", bookId)
+
+	// URL path is "/X/Y/Z/XYZW/XYZW.zip"
+	header := bookId[:len(bookId)-1]
+	runes := make([]rune, len(header)*2)
+	for i := range header {
+		runes[i*2] = '/'
+		runes[i*2+1] = rune(header[i])
 	}
-	if rsp.ContentLength == 0 {
-		return nil, errors.New("ContentLength 0")
+
+	// Fetch the book.
+	url := fmt.Sprintf("http://www.gutenberg.lib.md.us%v/%v/%v.zip", string(runes), bookId, bookId)
+	rsp, err := http.Get(url)
+	if err != nil || rsp.StatusCode != 200 {
+		// Some are stored as "xxxx-8.zip", presumably for "UTF-8".
+		url = fmt.Sprintf("http://www.gutenberg.lib.md.us%v/%v/%v-8.zip", string(runes), bookId, bookId)
+		rsp, err = http.Get(url)
+		if err != nil || rsp.StatusCode != 200 {
+			return nil, err
+		}
 	}
+
+	// Read the full response body.
 	defer rsp.Body.Close()
-	bytes, err := ioutil.ReadAll(rsp.Body)
+	zipBytes, err := ioutil.ReadAll(rsp.Body)
 	if err != nil {
 		return nil, err
 	}
 
+	// Unzip the first file and return that.
+	zr, err := zip.NewReader(bytes.NewReader(zipBytes), rsp.ContentLength)
+	if err != nil {
+		return nil, err
+	}
+	zfr, err := zr.File[0].Open()
+	if err != nil {
+		return nil, err
+	}
+	defer zfr.Close()
+	return ioutil.ReadAll(zfr)
+}
+
+func readBook(bookId string) (Book, error) {
+	// See if we have a local copy.
+	relPath := fmt.Sprintf("gutenberg/cache/%v.txt", bookId)
+	bytes, err := ioutil.ReadFile(relPath)
+	if err != nil {
+		// Fetch from Gutenberg.
+		bytes, err = readFromGutenberg(bookId)
+		if err != nil {
+			return nil, err
+		}
+
+		// Cache it locally.
+		err = ioutil.WriteFile(relPath, bytes, 0640)
+		if err != nil {
+			log.Printf("Error writing '%v': %v", relPath, err)
+			return nil, err
+		}
+	} else {
+		log.Printf("Read %v from cache", bookId)
+	}
+
+	// Process it into something palatable.
+	// TODO: Cache the processed form so we can memory map it or something similarly efficient.
 	text := strings.Replace(string(bytes), "\r\n\r\n", " <br> <br> ", -1)
 	text = strings.Replace(text, "\r\n", " ", -1)
 
@@ -51,19 +105,30 @@ func readBook(bookId string) (Book, error) {
 }
 
 func bookHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
+	// TODO: Make this stuff UTF-8 when downloading it.
+	//   The "-8" suffix might be intended as an indication of UTF-8.
+	w.Header().Add("Content-Type", "text/plain; charset=iso-8859-1")
+
 	bookId := r.URL.Query().Get("bookId")
 	firstPage, _ := strconv.ParseInt(r.URL.Query().Get("firstPage"), 10, 32)
 	pageCount, _ := strconv.ParseInt(r.URL.Query().Get("pageCount"), 10, 32)
 
 	book, exists := bookCache[bookId]
 	if !exists {
-		book, err := readBook(bookId)
+		var err error
+		book, err = readBook(bookId)
 		if err != nil {
+			log.Printf("Not found: %v", bookId)
 			http.NotFound(w, r)
 			return
 		}
 		bookCache[bookId] = book
+	}
+
+	if int(firstPage) < 0 || int(firstPage) >= len(book) || int(firstPage+pageCount) > len(book) {
+		log.Printf("Out of range (%v) : %v + %v", bookId, firstPage, pageCount)
+		http.NotFound(w, r)
+		return
 	}
 
 	for i := firstPage; i < firstPage+pageCount; i++ {

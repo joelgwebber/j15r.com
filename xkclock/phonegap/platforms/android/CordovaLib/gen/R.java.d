d2d5c7311ab11a0611055bce098e0dc3237db5e3
Joel Webber <jgw@pobox.com>
2014-03-01 16:39:57 -0500

    XKClock.

---
+++ xkclock/phonegap/platforms/android/CordovaLib/gen/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/jgw/src/j15r.com/xkclock/phonegap/platforms/android/CordovaLib/bin/AndroidManifest.xml \
---
+++ xkclock/phonegap/platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+/Users/jgw/src/j15r.com/xkclock/phonegap/platforms/android/gen/com/j15r/xkclock/R.java \
+ : /Users/jgw/src/j15r.com/xkclock/phonegap/platforms/android/res/drawable/icon.png \
+/Users/jgw/src/j15r.com/xkclock/phonegap/platforms/android/res/drawable-hdpi/icon.png \
+/Users/jgw/src/j15r.com/xkclock/phonegap/platforms/android/res/drawable-ldpi/icon.png \
+/Users/jgw/src/j15r.com/xkclock/phonegap/platforms/android/res/drawable-mdpi/icon.png \
+/Users/jgw/src/j15r.com/xkclock/phonegap/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/jgw/src/j15r.com/xkclock/phonegap/platforms/android/res/values/strings.xml \
+/Users/jgw/src/j15r.com/xkclock/phonegap/platforms/android/res/xml/config.xml \
+/Users/jgw/src/j15r.com/xkclock/phonegap/platforms/android/bin/AndroidManifest.xml \

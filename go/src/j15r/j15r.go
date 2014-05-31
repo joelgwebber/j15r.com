04afaeabb9f60e1ee43d839effe230db90e92df8
Joel Webber <jgw@pobox.com>
2014-05-31 11:59:33 -0400

    Finally stopped cheating and switched to typekit.

--- go/src/j15r/j15r.go
+++ go/src/j15r/j15r.go
@@ -60,6 +60,12 @@ const sharedTemplates = `
 {{define "head"}}
   <head>
     <title>{{.}}</title>
+    <script type="text/javascript">
+		(function(d) {
+			var config = { kitId: 'dlw6xba', scriptTimeout: 3000 },
+			h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+		})(document);
+		</script>
     <link rel='stylesheet' href='/s/j15r.css'>
 		{{template "fullstory-crap"}}
   </head>
